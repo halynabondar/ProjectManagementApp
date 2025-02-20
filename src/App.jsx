00dd7e319d00ac5f2fb17dc9d36e9a2ad2a30cae
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-02-20 14:43:30 +0100

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import ProjectsSidebar from './components/ProjectsSidebar';
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
+    const [projects, setProjects] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+    });
+
+    function handleAddProject() {
+        setProjects(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            }
+        });
+    }
+
+    let content;
+
+    if (projects.selectedProjectId === null) {
+        content = <NewProject />
+    } else if (projects.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleAddProject}/>;
+    }
+
   return (
-    <main className="h-screen">
-        <ProjectsSidebar />
+    <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar onStartAddProject={handleAddProject} />
+        {content}
     </main>
   );
 }
